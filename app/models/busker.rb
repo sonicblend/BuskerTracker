96e04916a9a131038db774a855f1666fe66ba2bb
Pete Black <pete.black@storm-consultancy.com>
2015-12-03 13:50:18 +0000

    make Busker name required, add test

--- app/models/busker.rb
+++ app/models/busker.rb
@@ -1,2 +1,3 @@
 class Busker < ActiveRecord::Base
+  validates :name, presence: true
 end
--- test/models/busker_test.rb
+++ test/models/busker_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class BuskerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @busker = Busker.new(name: "Loopy Tom",
+                         description: "Loop-based music",
+                         image_id: "http://www.mickyatesphotography.com/Candid-Street/The-People-of-Bath/i-hJKQWWs/0/XL/Leica%20Bath%20May%202014%20Colour%20Wash%20-2-XL.jpg",
+                         facebook: "https://www.facebook.com/loopytom",
+                         twitter: "https://twitter.com/loopytomdotcom",
+                         website: "http://loopytom.com/")
+  end
+
+  test "should be valid" do
+    assert @busker.valid?
+  end
+
+  test "name should be present" do
+    @busker.name = '   '
+    assert_not @busker.valid?
+  end
 end
