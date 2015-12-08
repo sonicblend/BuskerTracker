ad45c62de5eff4a12bc605f12ab9352585154b1b
Pete Black <pete.black@storm-consultancy.com>
2015-12-08 12:39:21 +0000

    Only store Twitter & Facebook identifier, not entire url. closes #4

--- test/models/busker_test.rb
+++ test/models/busker_test.rb
@@ -14,53 +14,36 @@ class BuskerTest < ActiveSupport::TestCase
     assert_not @busker.valid?
   end
 
-  test "twitter address should reject invalid urls" do
-    invalid_twitter_urls = %w[tweeter.com/blabla twitter.com/asdf.asdf]
+  test "twitter should reject invalid identifiers" do
+    invalid_twitter_urls = %w[tweeter.com/blabla twitter.com/asdf.asdf asdf.asdf]
     invalid_twitter_urls.each do |url|
       @busker.twitter = url
       assert_not @busker.valid?
     end
   end
 
-  test "twitter address should not be too long" do
+  test "twitter identifier should not be too long" do
     @busker.twitter = 'a' * 16
     assert_not @busker.valid?
   end
 
-  test "twitter address should be prefixed with https when no prefix specified" do
-    @busker.twitter = 'twitter.com/buskinginbath'
+  test "twitter url should be prefixed with https" do
+    @busker.twitter = 'buskinginbath'
     @busker.valid?
-    assert_equal('https://twitter.com/buskinginbath', @busker.twitter)
+    assert_equal('https://twitter.com/buskinginbath', @busker.twitter_url)
   end
 
-  test "twitter address should be prefixed with https when http prefix specified" do
-    @busker.twitter = 'http://twitter.com/buskinginbath'
-    @busker.valid?
-    assert_equal('https://twitter.com/buskinginbath', @busker.twitter)
-  end
-
-  test "facebook address should reject invalid urls" do
-    invalid_facebook_urls = %w[facebook.com/☃  fayzbook.com/bla facebook.com/bla_bla]
+  test "facebook should reject invalid identifiers" do
+    invalid_facebook_urls = %w[facebook.com/☃  fayzbook.com/bla facebook.com/bla_bla bla_bla ☃]
     invalid_facebook_urls.each do |url|
       @busker.facebook = url
       assert_not @busker.valid?
     end
   end
 
-  test "facebook address should not be too long" do
+  test "facebook identifier should not be too long" do
     @busker.facebook = 'a' * 51
     assert_not @busker.valid?
-  end
-
-  test "facebook address should validate fb.com urls" do
-    @busker.facebook = 'fb.com/SophieTheBusker'
-    assert @busker.valid?
-  end
-
-  test "facebook address should prefer https prefix and facebook.com" do
-    @busker.facebook = 'http://fb.com/SophieTheBusker'
-    @busker.valid?
-    assert_equal('https://facebook.com/SophieTheBusker', @busker.facebook)
   end
 
   test "website should be valid" do
