bf0f6bb83eb58e4862b05bebd7cd1fdb75013d13
Pete Black <pete.black@storm-consultancy.com>
2015-12-14 14:29:14 +0000

    ESLint fixes - refer to google maps using this so not referring to an undefined variable, trailing comma fix

--- app/assets/javascripts/home/map.js
+++ app/assets/javascripts/home/map.js
@@ -1,17 +1,19 @@
+/*eslint-disable no-unused-vars*/
 function load_storm_map() {
   var storm = {lat: 51.3829394, lng: -2.3607561000000032};
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new this.google.maps.Map(document.getElementById('map'), {
     center: storm,
     mapTypeControl: false,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeId: this.google.maps.MapTypeId.ROADMAP,
     zoom: 17
   });
 
-  var infowindow = new google.maps.InfoWindow({content:"<b>Storm Consultancy</b><br/>14 New Bond St<br/>Bath, BA1 1BE" });
-  var marker = new google.maps.Marker({
+  var infowindow = new this.google.maps.InfoWindow({content:"<b>Storm Consultancy</b><br/>14 New Bond St<br/>Bath, BA1 1BE" });
+  var marker = new this.google.maps.Marker({
     position: storm,
-    map: map,
+    map: map
   });
   infowindow.open(map, marker);
   map.setCenter(marker.getPosition());
 }
+/*eslint-enable no-unused-vars*/
