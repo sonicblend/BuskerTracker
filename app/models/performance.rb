1d81fc8d6bfb2de4917b965f782d1692e8f8961d
Pete Black <pete.black@storm-consultancy.com>
2016-01-04 15:27:07 +0000

    performance end_date must occur after the start_date

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -1,3 +1,10 @@
 class Performance < ActiveRecord::Base
   belongs_to :busker
+
+  validate :ensure_end_date_after_start_date
+
+  def ensure_end_date_after_start_date
+    errors.add(:end_date, "must occur after the start date") if
+      end_date.present? and end_date <= start_date
+  end
 end
--- test/models/performance_test.rb
+++ test/models/performance_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class PerformanceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @first = performances :first
+  end
+
+  test "should be valid" do
+    assert @first.valid?
+  end
+
+  test "start and end date must not be equal" do
+    @first.start_date = @first.end_date
+    assert_not @first.valid?
+  end
 end
