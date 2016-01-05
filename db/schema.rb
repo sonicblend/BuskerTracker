d79edf3167b767b5c2ab1d0506ffe9586c74490a
Pete Black <pete.black@storm-consultancy.com>
2016-01-05 10:31:55 +0000

    add locations to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151216095021) do
+ActiveRecord::Schema.define(version: 20160105100338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20151216095021) do
     t.string   "facebook"
     t.string   "twitter"
     t.string   "website"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
