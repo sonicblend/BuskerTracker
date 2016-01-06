97054f1a290d0f00372d973200bb6e30e79d0f33
Pete Black <pete.black@storm-consultancy.com>
2016-01-06 11:18:49 +0000

    add html_safe_if, required by the shared flash partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  # Pulic: html_safe a string if the condition is met
+  #
+  # params
+  #
+  #  condition  -  The condition to test
+  #  str        -  The string
+  #
+  def html_safe_if(condition, str)
+    if condition
+      str.html_safe
+    else
+      str
+    end
+  end
 end
