0ab9c8db033f2251a1ce5e6dbd960d362f5ca34b
Pete Black <pete.black@storm-consultancy.com>
2016-01-07 16:11:54 +0000

    enable devise :confirmable module

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,9 +15,11 @@ Rails.application.configure do
 
   # Mail configuration
   config.action_mailer.default_url_options = { host: 'busker-tracker.dev' }
+  config.action_mailer.smtp_settings       = { address: 'busker-tracker.dev', port: 1025 }
+  config.action_mailer.delivery_method     = :smtp
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Don't care if the mailer can't send?
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
